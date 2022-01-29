cb6e722f421179f84bd73bf26d7536d9daab7ae6
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2022-01-29 20:36:07 +0300

    fix linters

--- db/migrate/20220129170017_create_join_table_user_post.rb
+++ db/migrate/20220129170017_create_join_table_user_post.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableUserPost < ActiveRecord::Migration[7.0]
   def change
     create_join_table :users, :posts, column_options: { null: true }, table_name: :likes do |t|
-      t.index [:user_id, :post_id]
+      t.index %i[user_id post_id]
 
       t.timestamps
     end
